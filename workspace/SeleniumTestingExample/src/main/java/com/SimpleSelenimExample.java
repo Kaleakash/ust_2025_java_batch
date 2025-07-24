b2e8eb903c6ddd0c93fd66b8af683979f19b2cd4
Akash <akash300383@gmail.com>
2025-07-24 19:09:19 +0530

    done

--- workspace/SeleniumTestingExample/src/main/java/com/SimpleSelenimExample.java
+++ workspace/SeleniumTestingExample/src/main/java/com/SimpleSelenimExample.java
@@ -13,7 +13,7 @@ public class SimpleSelenimExample {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();		// created the reference of browser. 
 		driver.get("http://www.google.com");	  // using driver open the google page. 
-		driver.manage().window().maximize();		// maximize the web page screen
+			driver.manage().window().maximize();		// maximize the web page screen
 		String url = driver.getCurrentUrl();
 		String title = driver.getTitle();
 		String sourceCode = driver.getPageSource();
@@ -24,8 +24,12 @@ public class SimpleSelenimExample {
 		
 		WebElement submitButton = driver.findElement(By.name("btnK"));
 		submitButton.click();
-		Thread.sleep(3000);
+			Thread.sleep(3000);
 		driver.navigate().back();
+		Thread.sleep(3000);
+		textAreaRef.clear();
+		textAreaRef.sendKeys("What is Junit testing");
+		submitButton.click();
 		Thread.sleep(3000);
 		driver.navigate().forward();
 		Thread.sleep(3000);
