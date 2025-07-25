b1f1239315f6294a00228b2edfcd80942e0bba92
Akash <akash300383@gmail.com>
2025-07-25 18:17:44 +0530

    done

---
+++ workspace/Java8FeaturesExamples/src/com/DefaultAndStaticInterfaceExample.java
@@ -0,0 +1,34 @@
+package com;
+
+interface Test {
+	void dis1();		// by default abstract and public 
+	default void dis2() {
+		System.out.println("dis2 default method");
+	}
+	default void dis3() {
+		System.out.println("dis3 default method");
+	}
+	static void dis4() {
+		System.out.println("dis4 static method");
+	}
+}
+class TestImp implements Test{
+	public void dis1() {
+		System.out.println("abstract method provided body in TestImp");
+	}
+	@Override
+	public void dis2() {
+		System.out.println("dis2 default method override in TestImp");
+	}
+}
+public class DefaultAndStaticInterfaceExample {
+
+	public static void main(String[] args) {
+	TestImp tt1 = new TestImp();
+	tt1.dis1();
+	tt1.dis2();
+	tt1.dis3();
+	Test.dis4();  // static method 
+	}
+
+}
